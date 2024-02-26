6a4a8e782219f830daef7d07cbd6b2652b897c73
l-ouis <louisaltgeer@gmail.com>
2024-02-26 15:57:53 -0500

    Added basic gear-up code

---
+++ mock/src/components/REPLHistory.tsx
@@ -0,0 +1,14 @@
+import '../styles/main.css';
+
+interface REPLHistoryProps{
+    history: string[];
+}
+export function REPLHistory(props : REPLHistoryProps) {
+    return (
+        <div className="repl-history">
+            {/* This is where command history will go */}
+            {/* TODO: To go through all the pushed commands... try the .map() function! */}
+            {props.history.map((line) => <p>{line}</p>)}
+        </div>
+    );
+}
