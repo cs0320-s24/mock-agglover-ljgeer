b0cd80e093b098c37794410a2638e9c223f857ea
Ashton <ashton_glover@brown.edu>
2024-03-01 14:43:27 -0500

    feat: deleted command number next to verbose

--- mock/src/components/REPLHistory.tsx
+++ mock/src/components/REPLHistory.tsx
@@ -12,7 +12,7 @@ export function REPLHistory(props : REPLHistoryProps) {
             {/* TODO: To go through all the pushed commands... try the .map() function! */}
             {props.history.map((line, index) => 
                 props.outputMode === 'verbose' ? (
-                <p key={index}>Command {index + 1}: {line}</p>
+                <p key={index}>Command: {line}</p>
                 ) : (
                 <p key={index}>{line}</p>
                 )
