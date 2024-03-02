e749cb7e1a5481181ac76c6bab5a03cc65a5136d
Ashton <ashton_glover@brown.edu>
2024-03-02 12:16:29 -0500

    feat: filled in gearup tests

--- mock/tests/e2e/App.spec.ts
+++ mock/tests/e2e/App.spec.ts
@@ -60,9 +60,23 @@ test('on page load, i see a button', async ({ page }) => {
 });
 
 test('after I click the button, its label increments', async ({ page }) => {
-  // TODO WITH TA: Fill this in to test your button counter functionality!
+  await page.goto('http://localhost:8000/');
+  await page.getByLabel('Login').click();
+  await expect(page.getByRole('button', {name: 'Submitted 0 times'})).toBeVisible()
+  await page.getByRole('button', {name: 'Submitted 0 times'}).click()
+  await expect(page.getByRole('button', {name: 'Submitted 1 times'})).toBeVisible()
 });
 
 test('after I click the button, my command gets pushed', async ({ page }) => {
-  // TODO: Fill this in to test your button push functionality!
+  await page.goto('http://localhost:8000/');
+  await page.getByLabel('Login').click();
+  await page.getByLabel('Command input').fill('Awesome command');
+  await page.getByRole('button', {name: 'Submitted 0 times'}).click()
+
+  // you can use page.evaulate to grab variable content from the page for more complex assertions
+  const firstChild = await page.evaluate(() => {
+    const history = document.querySelector('.repl-history');
+    return history?.children[0]?.textContent;
+  });
+  expect(firstChild).toEqual("Awesome command");
 });
