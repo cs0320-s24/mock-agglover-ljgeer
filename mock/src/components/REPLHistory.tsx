ffcecfe6e18959004d18c7981ccdd7c4ec0aaf47
Ashton <ashton_glover@brown.edu>
2024-03-02 01:22:38 -0500

    fix: table centered

--- mock/src/components/REPLHistory.tsx
+++ mock/src/components/REPLHistory.tsx
@@ -11,16 +11,19 @@ export function REPLHistory(props: REPLHistoryProps) {
     return (
         <div className="repl-history">
             {props.history.map((line, index) => (
-                <div key={index}>
-                    {props.outputMode === 'verbose' ? (
-                        <div>
-                            <p>Command: {line}</p>  
-                            <span>Output: {props.commandOutput[index].print()}</span>
+                <div key={index} style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                {props.outputMode === 'verbose' ? (
+                    <div>
+                        <p>Command: {line}</p>  
+                        <div style={{ display: 'flex', alignItems: 'center' }}>
+                            <span>Output: </span>
+                            {props.commandOutput[index].print()}
                         </div>
-                    ) : (
-                        <p>{props.commandOutput[index].print()}</p>
-                    )}
-                </div>
+                    </div>
+                ) : (
+                    <p>{props.commandOutput[index].print()}</p>
+                )}
+            </div>
             ))}
         </div>
     );
