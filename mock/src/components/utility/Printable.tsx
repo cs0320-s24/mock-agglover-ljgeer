3e9b8d30383c49bc08c8007d1129912d498935ec
l-ouis <louisaltgeer@gmail.com>
2024-03-01 23:56:55 -0500

    fix: return type of Printables is now a react element

--- mock/src/components/utility/Printable.tsx
+++ mock/src/components/utility/Printable.tsx
@@ -1,4 +1,4 @@
 export interface Printable<T> { // Not sure about this generic type, can keep for now
     data: T;
-    print(): string; // Takes in an object's data-type and produces a string of printable HTML content
+    print(): JSX.Element; // Takes in an object's data-type and produces a JSX Element to print
 }
